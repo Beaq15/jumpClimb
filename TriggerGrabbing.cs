1f2757746e1ee75dd02abcec2379bf7c8562eea8
Beaq15 <91021012+Beaq15@users.noreply.github.com>
2022-01-19 23:54:53 +0200

    Add files via upload

---
+++ TriggerGrabbing.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerGrabbing : MonoBehaviour
+{
+    public GameObject rootPos;
+    void OnTriggerEnter(Collider obj)
+    {
+        obj.GetComponentInParent<ClimbUp>().GrabEdge(rootPos.transform);
+    }
+}
